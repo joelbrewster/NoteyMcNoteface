efb61eed9a7ab7fefa0b291e76b221c99ae67fdf
Joel <j.n.brewster@gmail.com>
2016-06-02 15:01:00 +1000

    Fix no content in title and content

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :notes
+  validates :email, :presence => true, :uniqueness => true
 end
